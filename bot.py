3b3179a609d0db1c74a47cf1b46ca3c9de6e7a2e
isaacli246over <72372215+isaacli246over@users.noreply.github.com>
2022-07-24 17:33:30 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from hikari import Intents
+
+
+Intents = discord.Intents.default()
+Intents.members = True
+
+bot=commands.Bot(command_prefix='[', intents = Intents)
+
+@bot.event
+async def on_ready():
+    print("bot is online")
+ 
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1000666712506650627)
+    await channel.send(f'{member} join!') 
+   
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1000666712506650627)
+    await channel.send(f'{member} leave!')
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+bot.run('MTAwMDY2ODcwODE2NDg4MjUyMg.G6zyxL.XpEGQoS4-5pcyoyvz94t4kqoFxvGf-XrWZUgtE')
